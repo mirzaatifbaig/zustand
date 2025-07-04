7c83467893bfe5ff91b1f065134a71ac866ac4b5
mirzaatifbaig <63869168+mirzaatifbaig@users.noreply.github.com>
2025-07-04 16:32:19 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/zustand/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
